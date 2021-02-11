eeae3d900285ef33ce29d9573c7c41a7a334bab8
korilium <ignace.decocq@hotmail.com>
2021-02-11 13:01:58 +0100

    second draft thesis

---
+++ _build/jupyter_execute/Intro.jl
@@ -0,0 +1,13 @@
+# Intro
+
+This is the jupyter notebook for the thesis in optimal consumption, invetsment and insurance. To read the paper please go to full thesis. 
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+
+
+Full thesis <Full_thesis>
+referencess
+```
